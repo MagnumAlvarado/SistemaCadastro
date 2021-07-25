44c4b2e352b219077d46ba85c6a82bbc405185c6
Magnum <magnum.alvarado@gmail.com>
2021-07-24 22:56:18 -0300

    COMMIT 10

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import react from 'react';
-import reactDOM from 'react-dom';
-import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
 import './index.css';
 
 

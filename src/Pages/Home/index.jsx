6b512d9b59b33a1d809f9f0c4ca320cbacc02199
Magnum <magnum.alvarado@gmail.com>
2021-07-24 21:09:37 -0300

    commit 2

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import Button from '../../components/Button';
+import Loader from '../../components/Loader';
+import { ButtonContainer, Container, Description, Title } from './styles';
+
+
+export default function Home() {
+  return (
+    <Container>
+      <Title>Gestão de Cadastros</Title>
+      <Description>Sistema para gestão de cadastros. Clique em Clientes para adicionar clientes na base de dados ou acione o botão de para adicionar produtos na base de dados.</Description>
+      <ButtonContainer>
+        <NavLink to='/clientes'><Button backColor='#117BB0'>Clientes</Button></NavLink>
+        <NavLink to='/produtos'><Button backColor='#FD5A57'>Produtos</Button></NavLink>
+      </ButtonContainer>
+      <Loader />
+    </Container>
+  )
+}
+© 2021 GitHub, Inc.

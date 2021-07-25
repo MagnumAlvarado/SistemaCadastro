39d1b9e0ba8a6a54c325157c20514d23677a2199
Magnum <magnum.alvarado@gmail.com>
2021-07-24 23:31:35 -0300

    commit18

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <Container>
       <Title>Sistema Cadastros</Title>
-      <Description>Sistema de gestão de cadastros. Clique em Clientes para adicionar clientes na base de dados ou acione o botão de para adicionar produtos na base de dados.</Description>
+      <Description>Sistema para inserir cadastros. Clique em Clientes para adicionar clientes na base de dados ou acione o botão de para adicionar produtos na base de dados.</Description>
       <ButtonContainer>
         <NavLink to='/clientes'><Button backColor='#117BB0'>Clients</Button></NavLink>
         <NavLink to='/produtos'><Button backColor='#FD5A57'>Produts</Button></NavLink>
